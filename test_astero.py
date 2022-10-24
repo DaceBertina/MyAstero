ab19058da9b5709384c894e435d6da4b0c69500c
Dace Ķelpe-Bērtiņa <kelpe.bertina@gmail.com>
2022-10-24 14:37:52 +0300

    Pievienojām pārbaudi ar "assert" un pytest.

--- test_astero.py
+++ test_astero.py
@@ -1,14 +1,17 @@
 from naked import *
 from datetime import datetime
+import re
+import unittest
 
 # Testing passing empty asteroids list
-print("Checking date format:")
-print("----------")
-print("Testing function: \get_todays_date\"")
-if datetime.strptime(get_todays_date(), "%Y-%m-%d"):
-    print("Date format is OK.")
-else:
-    print("Date is not OK.")
-print("----------")
+def test_date():
+    print("Checking date format:")
+    print("----------")
+    print("Testing function: get_todays_date()")
+    if datetime.strptime(get_todays_date(), "%Y-%m-%d"):
+        print("Date format is OK.")
+    else:
+        print("Date is not OK.")
+    print("----------")
 
-assert get_todays_date() == format('%Y-%m-%d')
+    assert re.match('\\d{4}-\\d{2}-\\d{2}', get_todays_date())
